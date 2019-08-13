f34f65bdd6fecad79b369ec939b619a06fd5af90
RJ Varona <varonarudson@gmail.com>
2019-08-13 11:46:12 -0400

    added post and delete

--- realsite/server/index.js
+++ realsite/server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const cors = require('cors');
+const port = 3000;
 
 const app = express();
 
@@ -13,8 +14,25 @@ const posts = require('./routes/api/posts');
 
 app.use('/api/posts', posts);
 
-const port = 3000;
+
 
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+
+
+// //connecting to database
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://elroiweick:elroiweick@cluster0-dhxyy.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("testcollection");
+// //   perform actions on the collection object
+
+//     var ins = {name: 'ellie', email: 'yeet@gmail.com'};
+//     collection.insertOne(ins, function(err, res){
+
+//     });
+//   client.close();
+// });
--- realsite/server/routes/api/posts.js
+++ realsite/server/routes/api/posts.js
@@ -4,15 +4,58 @@ const mongodb = require('mongodb');
 const router = express.Router();
 
 // Get Post
-router.get('/', (req, res) => {
-    res.send('hello');
+router.get('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    res.send(await posts.find({}).toArray());
 });
 
 
 // Add Post
+router.post('/', async (req, res) => {
+    const posts = await loadPostCollection();
+    await posts.insertOne({
+        text: req.body.text,
+        createdAt: newDate()
+    });
+    res.Status(201).send();
+});
+
+
 
 // Delete Post
+router.delete(':/id', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id)});
+    res.status(200).send();
+});
+
+
+
+//connectiong stuff
+async function loadPostsCollection() {
+    const client = await mongodb.MongoClient.connect
+    ('mongodb+srv://elroiweick:elroiweick@cluster0-dhxyy.mongodb.net/test?retryWrites=true&w=majority', {
+    usedNewParser: true
+
+    });
+    return client.db('test').collection('testcollection');
+}
 
 
 
 module.exports = router;
+
+// //connecting to database
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://elroiweick:elroiweick@cluster0-dhxyy.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("testcollection");
+// //   perform actions on the collection object
+
+//     var ins = {name: 'ellie', email: 'yeet@gmail.com'};
+//     collection.insertOne(ins, function(err, res){
+
+//     });
+//   client.close();
+// });
